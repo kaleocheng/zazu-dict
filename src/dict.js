59316dddb58ab0d13b09865b51357a099a66ae3f
Kaleo Cheng <kaleocheng@gmail.com>
2018-06-07 13:15:52 +0800

    Add code to run

---
+++ src/dict.js
@@ -0,0 +1,14 @@
+module.exports = (pluginContext) => {
+  return (search, env = {}) => {
+    return new Promise((resolve, reject) => {
+      resolve([
+        {
+          icon: 'fa-book',
+          title: 'Query Dict',
+          subtitle: 'Query Word for "' + search + '"',
+          value: search,
+        },
+      ]);
+    });
+  };
+};
